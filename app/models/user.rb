32a6690aec612717362e1ef3fd4404bb3d3e73e8
ken-wong <wangkun025@gmail.com>
2015-04-04 14:42:31 +0800

    Remove email for sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
-
-  attr_accessor :login
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:name]
 
   validates :name ,presence: true, uniqueness: { case_sensitive: false}
-
+  # attr_accessor :name
   # validates   :charge_rate, numericality: true
   belongs_to  :person
   has_many    :bookings
@@ -21,17 +19,7 @@ class User < ActiveRecord::Base
     User.order(:english_name).pluck(:english_name)
   end
 
-  private
-    def self.find_first_by_auth_conditions(warden_conditions)
-      conditions = warden_conditions.dup
-      if login = conditions.delete(:login)
-        where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-      else
-        if conditions[:name].nil?
-          where(conditions).first
-        else
-          where(name: conditions[:name]).first
-        end
-      end
-    end
+  def email_required?
+    false
+  end
 end
