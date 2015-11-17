f99f796dcb0c199c5bfb367f40d58dc928bf2652
ken-wong <wangkun025@gmail.com>
2015-11-17 14:40:30 +0800

    Remove restrict of receive_amount

--- app/models/receive_amount.rb
+++ app/models/receive_amount.rb
@@ -2,6 +2,4 @@ class ReceiveAmount < ActiveRecord::Base
   belongs_to  :billing
   validates :billing_id,      presence: true
   validates :receive_amount,  numericality: true
-  validates :invoice_no,      presence: true
-  validates :receive_date,    presence: true
 end
